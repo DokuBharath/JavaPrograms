27b4bc1f4cf0573514d4a8bee83359f35f4fd3bb
DokuBharath <d.bharath9493@gmail.com>
2022-06-03 15:18:54 +0530

    Example for String Comparasion

---
+++ JavaString/src/StringExample1.java
@@ -0,0 +1,12 @@
+
+public class StringExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="bharath";
+		String s2="Bharath";
+		System.out.println(s1==s2);
+
+	}
+
+}
